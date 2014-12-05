e2cff18fe5eef4cac365e553ca84c0d45029f2a7
Maxim Pechnikov <parallel588@gmail.com>
2014-12-05 22:45:37 +0300

    fix load version

--- lib/afalkear_translation_center.rb
+++ lib/afalkear_translation_center.rb
@@ -1,3 +1,4 @@
+require "translation_center/version"
 require "translation_center/engine"
 require "translation_center/acts_as_translator"
 require "translation_center/translation_helpers"
@@ -8,5 +9,3 @@ module TranslationCenter
   # add translations for translations center
   I18n.load_path += Dir[File.join(File.dirname(__FILE__), 'translation_center', 'locale', '*.yml')]
 end
-
-
